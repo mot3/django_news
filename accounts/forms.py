ad9c132a7f0783ae15854e8afc34aef48d88f957
mot3 <hopdrmot3@gmail.com>
2021-05-10 14:11:24 +0430

    some change and add pages app

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,12 @@
-from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import CustomUser
 
 class CustomeUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age',)
 
 class CustomeUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', 'age',)

96645ded26fdc75c97616b3332dcbfa3e60d2872
mot3 <hopdrmot3@gmail.com>
2021-05-11 19:01:58 +0430

    some change add article and comment

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # Local
     'accounts',
     'pages',
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'  
